6fcfc2011c4794744816bec17e94af37fef525df
Alexandria <lexluka1031@gmail.com>
2019-08-06 10:01:50 -0500

    Added material-ui to the project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import AboutMe from "../AboutMe/AboutMe";
+import Header from "../Header/Header";
+import Projects from "../Projects/Projects";
 
 const App: React.FC = () => {
-  return <div>App component</div>;
+  return (
+    <div>
+      <Header />
+      <AboutMe />
+      <Projects />
+    </div>
+  );
 };
 
 export default App;
