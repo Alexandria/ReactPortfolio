6e75ddd88a530fc19f6aba7f739bb9314e7355af
Alexandria <lexluka1031@gmail.com>
2019-08-06 10:00:49 -0500

    Add new util that holds all of my project info

---
+++ src/utils/myProjects/index.tsx
@@ -0,0 +1,24 @@
+import { Project } from "../../components/ProjectCard/ProjectCard";
+import typescript from "../../assests/icons/typescript.png";
+import Meeplely from "../../assests/images/Meeplely.jpg";
+import nodeJs from "../../assests/icons/nodeJs.png";
+import react from "../../assests/icons/react.png";
+import postgres from "../../assests/icons/postgres.png";
+
+export const myProjects: Project[] = [
+  {
+    title: "Meeplely",
+    img: Meeplely,
+    description:
+      "Full stack web application that will help you manage your board game collection.",
+    toolIcons: [typescript, nodeJs, react, postgres]
+  }
+];
+
+export const project: Project = {
+  title: "Meeplely",
+  img: Meeplely,
+  description:
+    "Full stack web application that will help you manage your board game collection.",
+  toolIcons: [typescript, nodeJs, react, postgres]
+};
