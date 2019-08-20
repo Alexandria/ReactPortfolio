b3c1b600939e3c1d7806af03f25d8df599095b07
Alexandria <lexluka1031@gmail.com>
2019-08-20 11:58:25 -0500

    Updates

---
+++ precache-manifest.45cf29c792af3352116b15d1c9bb3b4a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f9dae5f71600ca38f98bbea5f04340",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "fe3ae5e558a75655e38e",
+    "url": "/ReactPortfolio/static/css/main.530647ed.chunk.css"
+  },
+  {
+    "revision": "5eecaf63b7f2e4e9f112",
+    "url": "/ReactPortfolio/static/js/2.f86b1504.chunk.js"
+  },
+  {
+    "revision": "fe3ae5e558a75655e38e",
+    "url": "/ReactPortfolio/static/js/main.5c4aad8b.chunk.js"
+  },
+  {
+    "revision": "85e575e9d325a186e799",
+    "url": "/ReactPortfolio/static/js/runtime~main.2dc2a78e.js"
+  },
+  {
+    "revision": "7e90ce3657e2d97c1aef4854a5956ae5",
+    "url": "/ReactPortfolio/static/media/Meeplely.7e90ce36.png"
+  },
+  {
+    "revision": "02711c2cf037c786ebb844af1328f809",
+    "url": "/ReactPortfolio/static/media/django.02711c2c.jpg"
+  },
+  {
+    "revision": "674c121f52b4ff4924ca264c771f1d19",
+    "url": "/ReactPortfolio/static/media/insomnia.674c121f.png"
+  },
+  {
+    "revision": "eebad245108ae82e08d6e9017f4ccd4a",
+    "url": "/ReactPortfolio/static/media/nodeJs.eebad245.png"
+  },
+  {
+    "revision": "8b671c699f9bde2bceef5968593c1e8b",
+    "url": "/ReactPortfolio/static/media/phpmyadmin.8b671c69.png"
+  },
+  {
+    "revision": "ea92d6580dda0fbcf61a8ccb16e0d02a",
+    "url": "/ReactPortfolio/static/media/postgres.ea92d658.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/ReactPortfolio/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "cc4f3a678ae5249a1c5f693ba8233fa4",
+    "url": "/ReactPortfolio/static/media/typescript.cc4f3a67.png"
+  }
+]);
